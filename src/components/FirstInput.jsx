a9c2a400fdabeefdde78744aaaf47afa0b252b79
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-12-05 15:15:42 +0100

    Feat| esercizio 1

--- src/components/FirstInput.jsx
+++ src/components/FirstInput.jsx
@@ -3,18 +3,21 @@ import { useState } from "react";
 const FirstInput = () => {
   const [userName, setUserName] = useState("");
   const [isP, setIsP] = useState(false);
+  const [newName, setNewName] = useState("");
   return (
     <div>
       <input
         type="text"
-        value={userName}
+        value={newName}
         onChange={(e) => {
-          setUserName(e.target.value);
+          setNewName(e.target.value);
         }}
       />
       <button
         onClick={() => {
           setIsP(true);
+          setUserName(newName);
+          setNewName("");
         }}
       >
         Invia
